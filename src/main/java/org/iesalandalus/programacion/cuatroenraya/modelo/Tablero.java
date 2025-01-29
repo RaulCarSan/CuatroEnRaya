56eaca8b05d0281eda9895eedaf73d45aadf0de1
Raul Carmona Sanchez <racasan2002@gmail.com>
2025-01-29 11:13:03 +0100

    Fin de clase

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Tablero {
@@ -21,7 +22,7 @@ public class Tablero {
     }
 
     private boolean columnaLlena(int columna){
-        return tablero[FILAS + 1][COLUMNAS].estaOcupada();
+        return tablero[5][columna].estaOcupada();
     }
 
     public boolean estaVacio(){
@@ -34,16 +35,18 @@ public class Tablero {
         return ocupado;
     }
 
-    public boolean estaLleno(){
-        boolean vacio = true;
-        for (int fila = 0; fila < FILAS && !vacio; fila++){
-            for (int columna = 0; columna < COLUMNAS && !vacio; columna++){
-                if (tablero[fila][columna].estaOcupada()){
-                    vacio = false;
+    public boolean estaLleno() {
+        boolean vacio = false;
+        int columnasLlenas = 0;
+        for (int fila = 0; fila < FILAS; fila++){
+            for (int columna = 0; columna < COLUMNAS; columna++){
+                if (columnaLlena(columna)){
+                    columnasLlenas = columnasLlenas + 1;
+
                 }
             }
         }
-        return true;
+        return columnasLlenas == COLUMNAS;
     }
 
     private void comprobarFicha(Ficha ficha){
@@ -51,30 +54,36 @@ public class Tablero {
     }
 
     private void comprobarColumna(int columna){
-        if (columna <= 0){
-            throw new IllegalArgumentException("Columna incorrecta");
+        if (columna < 0){
+            throw new IllegalArgumentException("Columna incorrecta.");
         }
 
-        if (columna > 7){
+        if (columna >= 7){
             throw new IllegalArgumentException("Columna incorrecta.");
         }
     }
 
-    private int getPrimeraFilaVacia(int columna){
-        boolean vacia = columnaLlena(columna);
-        columna = -1;
+    private int getPrimeraFilaVacia(int columna) throws CuatroEnRayaExcepcion{
+        boolean vacia = false;
+        int filaInicial = -1;
         for (int fila = 0; fila < FILAS && !vacia; fila++){
-            vacia = columnaLlena(columna);
-            columna++;
+            if (!tablero[fila][columna].estaOcupada()){
+             filaInicial = fila;
+             vacia = true;
+            }
+
+        }
+        if (filaInicial == -1){
+            throw new CuatroEnRayaExcepcion("La fila esta llena.");
         }
         return columna;
     }
 
     private boolean comprobarHorizontal( int fila, Ficha ficha){
         int fichasSeguidas = 0;
-        for (; fila < FILAS && fichasSeguidas == 4; fila++){
+        for (; fila < FILAS && objetivoAlcanzado(fichasSeguidas); fila++){
             for (int columna = 0; columna < COLUMNAS; columna++){
-                if (tablero[fila][columna].estaOcupada() && ficha.equals(ficha)){
+                if (tablero[fila][columna].estaOcupada() && tablero[fila][columna].getFicha().equals(ficha)){
                     fichasSeguidas = 1 + fichasSeguidas;
                 }
             }
@@ -84,15 +93,15 @@ public class Tablero {
 
     private boolean comprobarVertical(int columna, Ficha ficha) {
         int fichasSeguidas = 0;
-        for (int fila = 0; fila < FILAS && fichasSeguidas == 4; fila++) {
+        for (int fila = 0; fila < FILAS && objetivoAlcanzado(fichasSeguidas); fila++) {
             for (; columna < COLUMNAS && tablero[fila][columna].estaOcupada(); columna++) {
-                if (tablero[fila][columna].estaOcupada() && ficha.equals(ficha)) {
+                if (tablero[fila][columna].estaOcupada() && tablero[fila][columna].getFicha().equals(ficha)) {
                     fichasSeguidas = 1 + fichasSeguidas;
                 }
             }
 
         }
-        return fichasSeguidas == FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS;
+        return objetivoAlcanzado(fichasSeguidas);
     }
 
     private int menor (int fila, int columna){
@@ -100,6 +109,76 @@ public class Tablero {
     }
 
     private boolean comprobarDiagonalNE(int filaActual, int columnaActual, Ficha ficha ){
-
+        int fichasSeguidas = 0;
+        int desplazamiento = menor(filaActual,columnaActual);
+        int filaInicial = filaActual - desplazamiento;
+        int columnaInicial = columnaActual - desplazamiento;
+        for (int fila = filaInicial, columna = columnaInicial;columnaActual < COLUMNAS && filaActual < FILAS || objetivoAlcanzado(fichasSeguidas);columnaActual--, filaActual--){
+            if (tablero[fila][columna].estaOcupada() && tablero[fila][columna].getFicha().equals(ficha)){
+                fichasSeguidas = fichasSeguidas + 1;
+            }
+        }
+        return objetivoAlcanzado(fichasSeguidas);
     }
+
+    private boolean comprobarDiagonalNO(int filaActual, int columnaActual, Ficha ficha ){
+        int fichasSeguidas = 0;
+        int desplazamiento = menor(filaActual,columnaActual);
+        int filaInicial = filaActual - desplazamiento;
+        int columnaInicial = columnaActual + desplazamiento;
+        for (int fila = filaInicial, columna = columnaInicial;columnaActual < COLUMNAS && filaActual < FILAS || objetivoAlcanzado(fichasSeguidas);columnaActual++, filaActual++){
+            if (tablero[fila][columna].estaOcupada() && tablero[fila][columna].getFicha().equals(ficha)){
+                fichasSeguidas = fichasSeguidas + 1;
+            }
+        }
+        return fichasSeguidas == FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS;
+    }
+
+    private boolean objetivoAlcanzado(int fichasIgualesConsecutivas){
+        return fichasIgualesConsecutivas == FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS;
+    }
+
+    private boolean comprobarTirada(int fila,int columna,Ficha ficha){
+        boolean victoria = false;
+        if (comprobarHorizontal(fila,ficha)){
+            victoria = true;
+        }
+
+        if (comprobarVertical(columna,ficha)){
+            victoria = true;
+        }
+
+        if (comprobarDiagonalNE(fila,columna,ficha)){
+            victoria = true;
+        }
+
+        if (comprobarDiagonalNO(fila,columna,ficha)){
+            victoria = true;
+        }
+
+        return victoria;
+    }
+
+    public boolean introducirFicha(int columna,Ficha ficha) throws CuatroEnRayaExcepcion{
+        boolean victoria;
+        comprobarColumna(columna);
+        comprobarFicha(ficha);
+        if (columnaLlena(columna)){
+            throw new IllegalArgumentException("Columna llena.");
+        }
+
+        do {
+            int fila = getPrimeraFilaVacia(columna);
+            victoria = comprobarTirada(fila,columna,ficha);
+            fila = fila + 1;
+        }while (!victoria);
+
+        return victoria;
+    }
+
+    //@Override
+    //public String toString() {
+      //  return String.format("[tablero=%s]", Arrays.toString(tablero));
+    //}
+
 }
